a13b447240b856aa580fb439ae901b678348844a
Loïc Pinçon <loic.pincon.ext@evollis.com>
2022-12-09 11:34:01 +0100

    add mongodb

--- jaccuzi-spa-back/src/main/java/fr/loicpincon/jaccuzispa/repository/InitSpa.java
+++ jaccuzi-spa-back/src/main/java/fr/loicpincon/jaccuzispa/repository/InitSpa.java
@@ -1,7 +1,9 @@
 package fr.loicpincon.jaccuzispa.repository;
 
 import fr.loicpincon.jaccuzispa.repository.entity.JavaSpaInformationsEntity;
+import fr.loicpincon.jaccuzispa.repository.entity.WifiInformationsEntity;
 import fr.loicpincon.jaccuzispa.repository.repository.SpaRepository;
+import fr.loicpincon.jaccuzispa.repository.repository.WifiRepository;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,12 +14,16 @@ public class InitSpa {
 
 
   private final SpaRepository spaRepository;
-
+  private final WifiRepository wifiRepository;
   @PostConstruct
   public void init() {
     if (spaRepository.findAll().isEmpty()) {
       JavaSpaInformationsEntity entity = new JavaSpaInformationsEntity();
       spaRepository.save(entity);
+    }
+    if (wifiRepository.findAll().isEmpty()) {
+      WifiInformationsEntity entity = new WifiInformationsEntity();
+      wifiRepository.save(entity);
     }
 
   }
