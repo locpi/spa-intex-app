998dedc66920ef1bad5f44d7e4d779f163f56c2b
Loïc Pinçon <loic.pincon.ext@evollis.com>
2022-12-20 10:06:56 +0100

    fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,3 +3,9 @@ git pull
 mvn -f jaccuzi-spa-back/pom.xml clean package
 mvn -f jaccuzi-spa-back/pom.xml jib:dockerBuild
 
+# shellcheck disable=SC2164
+cd jaccuzi-control
+
+npm install
+ng build -c production
+
