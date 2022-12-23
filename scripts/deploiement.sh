f7a62a3b1ebd4c4fac75357d57cb120b16babcb2
Loïc Pinçon <loic.pincon.ext@evollis.com>
2022-12-23 09:35:14 +0100

    fix

--- scripts/deploiement.sh
+++ scripts/deploiement.sh
@@ -2,5 +2,6 @@
 #docker-compose -f deploy/prod/docker-compose.yml up app-bitbucket -d
 
 docker-compose -f docker-compose.yml rm -s -f -v app
+docker-compose -f docker-compose.yml rm -s -f -v front
 docker-compose -f docker-compose.yml up app -d
 docker-compose -f docker-compose.yml up front -d
