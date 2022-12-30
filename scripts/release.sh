a0ff6ef234da5bb1d43da5ed3ee58697d6423379
Loïc Pinçon <loic.pincon.ext@evollis.com>
2022-12-30 13:18:11 +0100

    upgrade release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,9 @@
-VERSION=2.0
+source .env
 
 cd  back &&
 mvn clean package &&
-docker buildx build --platform linux/amd64 -t loicpincon/spa-intex-back:$VERSION --push .
+docker buildx build --platform linux/amd64 -t loicpincon/spa-intex-back:$VERSION_BACK --push .
 
 cd ../front &&
 ng build -c production &&
-docker buildx build --platform linux/amd64 -t loicpincon/spa-intex-front:$VERSION --push .
+docker buildx build --platform linux/amd64 -t loicpincon/spa-intex-front:$VERSION_FRONT --push .
